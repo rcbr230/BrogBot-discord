11dac27ea33a82679375e6cdf1dde32e32bb3f53
Reece Brogden <reece.brogden@outlook.com>
2025-08-06 15:54:16 +0000

    added help cmd

--- src/bot.py
+++ src/bot.py
@@ -8,17 +8,26 @@ intents.message_content = True  # Required to read message content
 # Prefix for bot commands
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# help text:
+help_text = """
+help - display all valid commands and how to use them
+ping - pong!
+"""
+
 @bot.event
 async def on_ready():
-    print(f"Bot is online as {bot.user}")
+    	print(f"Bot is online as {bot.user}")
 
+#ping
 @bot.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    	await ctx.send("Pong!")
 
+#help
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f"Hello, {ctx.author.name}!")
+async def help():
+	await ctx.send(help_text)
+
 
 # Replace YOUR_BOT_TOKEN with your actual token
 bot.run("YOUR_BOT_TOKEN")
